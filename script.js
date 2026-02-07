cea8432877eaa7de7d9a8a8a84663d99a4d296d7
arupb6002 <1arupborah2@gmail.com>
2026-02-07 04:50:53 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 console.log("Bezbaruahns Coming Soon loaded");
+(function () {
+  emailjs.init("KVgZayqX8kWFjp3eD"); // yaha public key aayega
+})();
 
+document.getElementById("suggestForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_8ifch4p",
+    "template_72zdsjm",
+    this
+  ).then(
+    function () {
+      document.getElementById("statusMsg").innerText =
+        "✅ Thank you! Your suggestion has been sent.";
+      document.getElementById("suggestForm").reset();
+    },
+    function (error) {
+      document.getElementById("statusMsg").innerText =
+        "❌ Something went wrong. Please try again.";
+      console.error(error);
+    }
+  );
+});
 document.addEventListener('DOMContentLoaded', function () {
   new Splide('#image-slider', {
     type: 'loop',
